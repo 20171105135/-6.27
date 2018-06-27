8fb7ee46d60e5ba1c5e495df7896e29afdd68f7c
20171105135 <1115828778@qq.com>
2018-06-27 11:03:17 +0800

    Initial Commit

---
+++ 评委打分/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  评委打分
+//
+//  Created by 王皎 on 18/6/27.
+//  Copyright © 2018年 王皎. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
